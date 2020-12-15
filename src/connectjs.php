80c50cbfd1b00137522ca7ac6d3fb193cb47f1d2
YuliyaMilk <YuliyaIse@gmail.com>
2020-12-15 16:48:13 +0600

    add js file

---
+++ src/connectjs.php
@@ -0,0 +1,6 @@
+<!-- jQuery Library  -->
+<script src="js/jquery.js"></script>
+<!-- Bootstrap default js отвечает за открытии окошка записи-->
+<script src="js/bootstrap.min.js"></script> 
+<!-- Custom JS -->
+<script src="js/custom.js"></script>

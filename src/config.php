29e07d2083ade477f3d2e861eef01bae124179a7
IrinaS-77 <irinaiv635@gmail.com>
2020-12-16 17:17:13 +0300

    config

--- src/config.php
+++ src/config.php
@@ -1,2 +1,6 @@
 <?php
 $con = mysqli_connect("localhost","root","", "database") or die(mysqli_error($con));
+if(!$con){
+    echo' Ошибка соединения: ' . mysqli_connect_error() . '<br>';
+    echo' Код ошибки: ' . mysqli_connect_errno() ;
+}
